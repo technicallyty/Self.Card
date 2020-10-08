b11127f8bd009693f2a0d7567be031291c433844
technicallyty <48813565+tytech3@users.noreply.github.com>
2020-10-07 17:30:28 -0700

    boilerplate
    many plates a boiling, the boiling be the plates.

--- SelfCard/auth.py
+++ SelfCard/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 
@@ -9,10 +12,49 @@ auth = Blueprint('auth', __name__)
 def login():
   return render_template('login.html')
 
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+  email = request.form.get('email')
+  password = request.form.get('password')
+
+  user = User.query.filter_by(email=email).first()
+  
+  if not user or not check_password_hash(user.password, password):
+    flash('Please check your login details and try again.')
+    return redirect(url_for('auth.login'))
+
+  login_user(user)
+  return redirect(url_for('main.profile'))
+
+
 @auth.route('/signup')
 def signup():
   return render_template('signup.html')
 
+
+@auth.route('/signup', methods=['POST'])
+def sign_post():
+  email = request.form.get('email')
+  name = request.form.get('name')
+  password = request.form.get('password')
+
+  user = User.query.filter_by(email=email).first()  # if this returns a user, then the email already exists in db
+
+  if user:
+    flash('Email address already exists')
+    return redirect(url_for('auth.signup'))
+
+  new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+  db.session.add(new_user)
+  db.session.commit()
+
+  return redirect(url_for('auth.login'))
+
+
 @auth.route('/logout')
+@login_required
 def logout():
-  return 'Logout'
+  logout_user()
+  return redirect(url_for('main.index'))
